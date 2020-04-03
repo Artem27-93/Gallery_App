4839914821becc048e1e5850735128c98aa2462f
Artem27-93 <artem27negoda@gmail.com>
2020-04-03 12:36:17 +0300

    add filter tabs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import MoviesData from '../moviesData';
 import MovieItem from '../MovieItem';
 import {API_URL,API_KEY_3} from '../../api';
+import MovieTabs from '../MovieTabs';
 
 
 class App extends Component {
@@ -9,14 +10,15 @@ class App extends Component {
         super();
         this.state = {
             movies: MoviesData,
-            moviesWillWatch: []
+            moviesWillWatch: [],
+            sort_by:'popularity.desc'
         }
         console.log("constructor")
     }
 
     componentDidMount(){
         console.log("didMount")
-        fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=popularity.desc`).then((response)=>{
+        fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`).then((response)=>{
             console.log('then')
             return response.json()
         }).then((data)=>{ 
@@ -54,12 +56,27 @@ class App extends Component {
             moviesWillWatch: updateMoviesWillWatch  
         })
     }
+
+    updateSortBy = value =>{
+        this.setState({
+            sort_by: value
+        });
+    }
   render(){
     console.log("render")
     return (
             <div className="container">
-                <div className="row">
+            <div className="container row mt-4">
+                <h1  style={{color:"maroon"}}>Gallery</h1>
+            </div>
+            
+                <div className="row mt-4">
                     <div className="col-9">
+                        <div className="row mb-4">
+                             <div className="col-12">
+                                <MovieTabs sort_by={this.state.sort_by} updateSortBy= {this.updateSortBy} />
+                            </div> 
+                        </div>
                         <div className="row">
                             {this.state.movies.map(movie=>{
                                 
